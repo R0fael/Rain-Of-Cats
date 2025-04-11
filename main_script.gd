d0c7426f708f3fe0190d1eaf81d01da1771489a3
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-04-11 16:13:24 -0500

    Update main_script.gd

--- main_script.gd
+++ main_script.gd
@@ -17,8 +17,8 @@ func _on_button_storm_button_down() -> void:
 func show_a_cat() -> void:
 	var rand_y: float = randf() * 300 + 200
 	var rand_x: float = randf() * 600 + 300
-	print("Y pos = ",rand_y)
-	print("X pos = ",rand_x)
+	#print("Y pos = ",rand_y)
+	#print("X pos = ",rand_x)
 	if cat_num < cat_max:
 		cat_num += 1
 		var cat := %Control_Cats.find_child("Button_Cat_" + str(cat_num))
