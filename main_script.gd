eb1415f1e155293d60fb1b15717be6b0bf5e0154
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-04-11 18:36:19 -0500

    cats fall

--- main_script.gd
+++ main_script.gd
@@ -1,30 +1,63 @@
 extends Control
 
-var cat_num: int = 0
-var cat_max: int = 10
+var cat_num: int = -1
+var cat_max: int = 5
+
+var cat_fall_amounts: Array = [
+	0.,
+	0.,
+	0.,
+	0.,
+	0.,
+	0.,
+]
+var difficulty_scalar: float = 2.
+var fall_amount: float = 0.0
+var float_amount: float = 1.
+
+var storm_button_pos_initial: float = 464.
+var storm_button_pos_away: float = 700.
 
 func _init() -> void:
 	print("h3llo")
 
+func _input(input) -> void:
+	adjust_cats_pos()
+	
+func adjust_cats_pos():
+	if cat_num > -1:
+		for i: int in cat_num + 1:
+			print(i)
+			cat_fall_amounts[cat_num] += fall_amount
+			var cat : Control = %Control_Cats.find_child("Control_Cat_" + str(i))
+			#print(cat)
+			cat.position.y += fall_amount
+			
+		
 
 
 func _on_button_storm_button_down() -> void:
+	%Button_Storm.position.y = storm_button_pos_away
+	
+	fall_amount += difficulty_scalar
+	
 	for i in int(100):
-		print("goodby")
 		await gui_input
+		#print("goodby")
 		show_a_cat()
 		
 func show_a_cat() -> void:
-	var rand_y: float = randf() * 300 + 200
-	var rand_x: float = randf() * 600 + 300
-	#print("Y pos = ",rand_y)
-	#print("X pos = ",rand_x)
-	if cat_num < cat_max:
-		cat_num += 1
-		var cat := %Control_Cats.find_child("Button_Cat_" + str(cat_num))
-		cat.position.y = rand_y
-		cat.position.x = rand_x
+	if not cat_num >= cat_max:
 		
-	if cat_num >= cat_max:
-		print("out of cats")
-	
+		var rand_y: float = randf() * 300 + 300
+		var rand_x: float = randf() * 600 + 300
+		#print("Y pos = ",rand_y)
+		#print("X pos = ",rand_x)
+		if cat_num < cat_max:
+			cat_num += 1
+			var cat : Control = %Control_Cats.find_child("Control_Cat_" + str(cat_num))
+			cat.position.y = rand_y
+			cat.position.x = rand_x
+			
+		
+		
